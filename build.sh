5d1b2a9304e16b9d05115dede77ba00c8d730014
xiao de <1558249222@qq.com>
2025-03-05 23:23:15 +0800

    add note

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 conan install . --output-folder=build --build=missing  # 使用 Conan 安装依赖并输出到 build 目录
 
 cd build  # 进入 build 目录
-cmake ..  # 运行 CMake 配置项目，生成构建系统文件
+cmake ..  # 运行 CMake 配置项目，进入项目的上一级目录， 查找CMakeLists.txt，生成构建系统文件
 make  # 使用 Make 构建项目
 
 cmake --build . --config Release  # 使用 CMake 构建项目，指定 Release 配置

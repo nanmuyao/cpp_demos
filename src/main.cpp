b2956ff34da9dcd55c9e1e5477f3f2012763a40d
xiao de <1558249222@qq.com>
2025-03-06 23:46:47 +0800

    feat: add debug option

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,20 @@ using namespace std;
 
 #include "zlib.h"
 
+template <typename T>
+T max(T a, T b) {
+  if (a > b) {
+    return a;
+  } else {
+  return b;
+}
+}
+
+
 int main(void) {
   std::cout << "zlib version: " << zlibVersion() << std::endl;
 
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "abc" << std::endl;
+
   return 0;
 }
